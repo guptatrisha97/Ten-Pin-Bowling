49e3941786f438b51b8ddc00b175dac2a812fe75
guptatrisha97 <35959848+guptatrisha97@users.noreply.github.com>
2018-02-02 11:02:19 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,48 +1,77 @@
-var TenPin = function() {
-	this.rolls = [];
-	this.presentRoll = 0;
-};
+<!DOCTYPE html>
+<html>
 
-TenPin.prototype.roll = function(pins) {
-	this.rolls[this.presentRoll++] = pins;
-};
-
-TenPin.prototype.score = function() {
-	var score = 0;
-	var frameIndex = 0;
-	var self = this;
-
-	function sumOfBallsInFrame() {
-		return self.rolls[frameIndex] + self.rolls[frameIndex + 1];
-	}
-
-	function spareBonus() {
-		return self.rolls[frameIndex + 2];
-	}
-
-	function strikeBonus() {
-		return self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2];
-	}
-
-	function isStrike() {
-		return self.rolls[frameIndex] === 10;
-	}
-
-	function isSpare() {
-		return self.rolls[frameIndex] + self.rolls[frameIndex + 1] === 10;
-	}
-
-	for (var frame = 0; frame < 10; frame++) {
-		if (isStrike()) {
-			score += 10 + strikeBonus();
-			frameIndex++;
-		} else if (isSpare()) {
-			score += 10 + spareBonus();
-			frameIndex += 2;
-		} else {
-			score += sumOfBallsInFrame();
-			frameIndex += 2;
-		}
-	}
-	return score;
-};
+<head>
+    <title>BOWLING SCORE</title>
+    
+</head>
+<body>
+   
+    <button onclick="score();">Click to input</button>
+    
+    <p id="demo"></p>
+</body>
+<script>
+    
+    function spare_bonus(s,i){
+        if((i+1)>=s.length){
+            return 0;
+        }
+        else
+        {
+            return s[i+1];
+        }
+    }
+    function strike_bonus(s,i){
+        if((i+2)>=s.length){
+            return 0;
+        }
+        else
+        {
+            return s[i+1]+s[i+2];
+        }
+    }
+    function score(){
+        
+        var str=prompt("Please enter the frames");
+        var ss = str.split(",");
+        var s = [];
+        var i;
+        for(i=0;i<ss.length;i++){
+            s.push(parseInt(ss[i]));
+        }        
+        var c=0;
+        var score=0;
+        var frame=0;
+        for(i=0;i<s.length;i++)
+        {
+            if((i+1)<s.length & frame<10)
+            {
+                if((s[i]+s[i+1])==10)
+                {
+                    score=score+10+spare_bonus(s,i+1);
+                    i++;
+                }
+                else if(s[i]==10)
+                {
+                    score=score+10+strike_bonus(s,i);
+                }
+                else
+                {
+                    score=score+(s[i]+s[i+1]);
+                    i=i+1;
+                }
+                frame++;
+            }
+            else if(frame<10)
+            {
+                score=score+s[i];
+            }
+            else{}
+        }
+        
+        document.getElementById("demo").innerHTML ="SCORE = "+score;
+    }
+    
+</script>
+</html>    
